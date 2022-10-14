39fd8c9b3901f7a2add69dee100235d2f9fce0d9
Victor504 <victor.rasmussen.vr@gmail.com>
2022-10-14 11:20:51 +0200

    Add project files.

---
+++ Närträff_2_1/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+   
+
+namespace Närträff_2_1
+{
+    internal class Person
+    {
+        private string namn, adress, tele;
+        public int NumbPer = 0;
+        public Person[] list = new Person[2];
+
+        public Person(string namn, string adress, string tele)
+        {
+            this.namn = namn;
+            this.adress = adress;
+            this.tele = tele;
+        }
+        public string Name
+        {
+            get { return namn; }
+            set { namn = value; }
+        }
+
+        public string Tele
+        {
+            get { return tele; }
+            set { tele = value; }
+        }
+
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+        public void runClassAdd()
+        {
+            AddDynamiskPerson();
+            PrintListDynamik();
+        }
+        public void AddDynamiskPerson()
+        {
+            PrintListDynamik();
+            WriteLine("ADD PERSON TO PHONEBOOK");
+            Write("NAME: ");
+            Name = ReadLine();
+            Write("ENTER A ADRESS: ");
+            Adress = ReadLine();
+            Write("ENTER A PHONENUMBER: ");
+            Tele = ReadLine();
+            list[NumbPer] = new Person(Name,Adress,Tele);
+            NumbPer++;
+        }
+        public void PrintListDynamik()
+        {
+            foreach (Person person in list)
+            {
+                WriteLine(person);
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format($"Namn: {namn} Adress: {adress} Telefonnummer: {tele}");
+        }
+    }
+}
---
+++ Närträff_2_1/Program.cs
@@ -0,0 +1,134 @@
+﻿using Närträff_2_1;
+using System;
+using System.Reflection;
+using static System.Console;
+
+namespace Program
+{
+    class prog
+    {
+
+        /*För att listan bara ska kunna ta classen
+        public Person<Person>[];*/
+
+        static void StartLogo()
+        {
+            WriteLine("==========================");
+            WriteLine("WELCOME TO YOUR PHONEBOOK");
+            WriteLine("==========================");
+        }
+        static void OptionMenu()
+        {
+            WriteLine("[1] ADD CONTACT");
+            WriteLine("[5] ADD DYNAMIK");
+            WriteLine("[2] DISPLAY CONTACT");
+            WriteLine("[6] DISPLAY DYNAMIK");
+            WriteLine("[3] REMOVE CONTACT");
+            WriteLine("[4] EXIT");
+
+        }
+     
+        static void AddPerson(Person[] persons)
+        {
+            DisplayList(persons);
+
+            WriteLine("DEDICATE ADRESS TO A NUMBER");
+            Write("OPTION [1] TO [10]: ");
+            try
+            {
+                int index = Convert.ToInt32(ReadLine());
+                persons[index] = new Person(null,null,null);
+                Write("ENTER A NAME: ");
+                persons[index].Adress = ReadLine();
+
+                Write("ENTER A ADRESS: ");
+                persons[index].Adress = ReadLine();
+
+                Write("ENTER A PHONENUMBER: ");
+                persons[index].Tele = ReadLine();
+
+            }
+            catch
+            {
+                WriteLine("ENTER A NUMBER");
+                WriteLine("PRESS ENTER TO CONTINUE");
+                ReadKey();
+            }
+          }
+        
+        static void DisplayList(Person[] persons)
+        {
+            int x = 0;
+            foreach (Person var in persons)
+            {
+                Write(x + " ");
+                WriteLine(var);
+                x++;
+            }
+        }
+
+        static void RemovePerson(Person[] persons)
+        {
+
+            Write("ENTER NUMBER OF ADRESS TO REMOVE: ");
+            try
+            {
+                int input = Convert.ToInt32(ReadLine());
+                persons[input] = new Person(null,null,null);
+            }
+            catch
+            {
+                WriteLine("ENTER A NUMBER");
+                ReadKey();
+            }
+        }
+
+        static void Run()
+        {
+
+            Person[] persons = new Person[10];
+            string input;
+            StartLogo();
+            OptionMenu();
+            do
+            {
+                Write("CHOSE A OPTION: ");
+                input = ReadLine();
+                if (input == "1")
+                {
+                    Clear();
+                    AddPerson(persons);
+                }
+                else if (input == "2")
+                {
+                    Clear();
+                    DisplayList(persons);
+                    Write("PRESS ENTER");
+                    ReadKey();
+                    Clear();
+                }
+                else if (input == "3")
+                {
+                    DisplayList(persons);
+                    RemovePerson(persons);
+                    Clear();
+                }
+                else if (input == "5")
+                {
+                }
+                else
+                {
+                    WriteLine("BYE BYE");
+                }
+                StartLogo();
+                OptionMenu();
+            }
+            while (input != "7");
+        }
+        static void Main(string[] args)
+        {
+            
+            Run();
+        }
+    }
+}
